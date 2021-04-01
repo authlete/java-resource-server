b143774a57786a2337843d55f219f4da22ce3638
Takahiko Kawasaki <taka@authlete.com>
2021-04-01 13:47:50 +0900

    [openbanking] Prepend "{ClientId}:" to Account Request ID when possible

--- src/main/java/com/authlete/jaxrs/server/api/openbanking/AccountRequestsEndpoint.java
+++ src/main/java/com/authlete/jaxrs/server/api/openbanking/AccountRequestsEndpoint.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Authlete, Inc.
+ * Copyright (C) 2018-2021 Authlete, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,10 +25,13 @@ import javax.ws.rs.DefaultValue;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import com.authlete.common.api.AuthleteApiFactory;
 import com.authlete.common.util.Utils;
+import com.authlete.jaxrs.AccessTokenInfo;
 import com.authlete.jaxrs.BaseResourceEndpoint;
 
 
@@ -43,7 +46,41 @@ public class AccountRequestsEndpoint extends BaseResourceEndpoint
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     public Response post(
-            @HeaderParam("x-fapi-interaction-id") @DefaultValue("") String interactionId)
+            @HeaderParam(HttpHeaders.AUTHORIZATION) String authorization,
+            @HeaderParam("x-fapi-interaction-id") @DefaultValue("") String incomingInteractionId)
+    {
+        // Process the access token.
+        AccessTokenInfo atInfo = processAccessToken(authorization);
+
+        // Prepare the content of the response.
+        Map<String, Object> content = buildContent(atInfo);
+
+        // Prepare the outgoing interaction ID.
+        String outgoingInteractionId = buildInteractionId(incomingInteractionId);
+
+        // Build the response.
+        return buildResponse(content, outgoingInteractionId);
+    }
+
+
+    private AccessTokenInfo processAccessToken(String authorization)
+    {
+        // Extract an access token from the Authorization header.
+        String accessToken = extractAccessToken(authorization, null);
+
+        // If the request does not contain an access token.
+        if (accessToken == null)
+        {
+            // Hmm. This should not happen in production environments.
+            return null;
+        }
+
+        // Validate the access token.
+        return validateAccessToken(AuthleteApiFactory.getDefaultApi(), accessToken);
+    }
+
+
+    private Map<String, Object> buildContent(AccessTokenInfo atInfo)
     {
         // {
         //   "Data" : {
@@ -51,26 +88,57 @@ public class AccountRequestsEndpoint extends BaseResourceEndpoint
         //   }
         // }
 
-        Map<String, Object> root = new HashMap<String, Object>();
+        Map<String, Object> content = new HashMap<String, Object>();
 
         // Data
         Map<String, Object> data = new HashMap<String, Object>();
-        root.put("Data", data);
+        content.put("Data", data);
+
+        // Generate an account request ID.
+        String accountRequestId = generateAccountRequestId(atInfo);
 
         // Data.AccountRequestId
-        data.put("AccountRequestId", UUID.randomUUID().toString());
+        data.put("AccountRequestId", accountRequestId);
 
-        // Generate the outgoing interaction ID if necessary.
-        if (interactionId == null || interactionId.isEmpty())
+        return content;
+    }
+
+
+    private String generateAccountRequestId(AccessTokenInfo atInfo)
+    {
+        String random = UUID.randomUUID().toString();
+
+        // If information about the access token is not available.
+        if (atInfo == null)
         {
-            interactionId = UUID.randomUUID().toString();
+            return random;
         }
 
+        // Prepend "{ClientId}:".
+        return String.format("%d:%s", atInfo.getClientId(), random);
+    }
+
+
+    private String buildInteractionId(String incomingInteractionId)
+    {
+        if (incomingInteractionId != null && !incomingInteractionId.isEmpty())
+        {
+            // Embed the same interaction ID in the response.
+            return incomingInteractionId;
+        }
+
+        // Generate a new interaction ID.
+        return UUID.randomUUID().toString();
+    }
+
+
+    private Response buildResponse(Map<String, Object> content, String interactionId)
+    {
         // 201 Created, application/json
         return Response
                 .status(Status.CREATED)
                 .type(MediaType.APPLICATION_JSON_TYPE)
-                .entity(Utils.toJson(root, true))
+                .entity(Utils.toJson(content, true))
                 .header("x-fapi-interaction-id", interactionId)
                 .build();
     }
